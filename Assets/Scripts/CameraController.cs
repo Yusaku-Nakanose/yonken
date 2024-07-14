9a315a14e3612637cc409d319f5f63ed640d172d
Yusaku Nakanose <niswog.daruma@gmail.com>
2024-07-14 13:12:45 +0900

    Improve CameraController script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,24 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public GameObject target = null;
+    public float height = 3f;
+    public float distance = 4f;
 
-    void Start()
+    void LateUpdate()
     {
+        if (target != null)
+        {
+            // ターゲットの位置と回転を取得
+            Vector3 targetPosition = target.transform.position;
+            Quaternion targetRotation = target.transform.rotation;
 
-    }
+            // カメラの位置を計算
+            Vector3 position = targetPosition - (targetRotation * Vector3.forward * distance);
+            position.y = targetPosition.y + height;
 
-    void Update()
-    {
-        Vector3 position = target.transform.position;
-        Quaternion rotation = target.transform.rotation;
-        position.y += 3f;
-        position.z -= 4f;
-        rotation.x = 0.1f;
-        transform.position = position;
-        transform.rotation = rotation;
+            // カメラの位置と回転を適用
+            transform.position = position;
+            transform.LookAt(targetPosition + Vector3.up * height / 2);
+        }
     }
 }
